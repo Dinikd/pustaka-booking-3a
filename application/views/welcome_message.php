4f34e016ba944e4bc69b75e7679e05ebc4adf62f
Dinikd <dinikd25@gmail.com>
2021-09-21 16:39:42 +0700

    first commit

---
+++ application/views/welcome_message.php
@@ -0,0 +1,21 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<body>
+
+<div id="container">
+	<h1>Perkenalan</h1>
+
+	<div id="body">
+		<p>Nama : Dini Kusuma Dewi</p>
+		<p>Tanggal Lahir : 25 Oktober 2001</p>
+		<p>Tempat Tinggal : Bekasi</p>
+		<p>Motivation Kuliah : bekerja di perusahaan terutama bidang IT</p>
+		
+	</div>
+
+</div>
+
+</body>
+</html>
