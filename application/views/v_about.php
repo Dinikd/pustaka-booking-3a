32cd1f990a4557dfdf0389122c883fc2c3f47eb9
Dinikd <dinikd25@gmail.com>
2021-10-06 19:55:05 +0700

    simple web templated

---
+++ application/views/v_about.php
@@ -0,0 +1,8 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <p>
+        Halaman About<br>
+        Nama        : Dini Kusuma Dewi <br>
+        Alamat      : Jl. Dream Treasure no 127
+    </p>
+</section>
---
+++ application/views/v_index.php
@@ -0,0 +1,17 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque molestias, voluptatum repellat, sunt deleniti, deserunt delectus voluptatibus 
+            aut ratione nesciunt a! Et blanditiis nobis consectetur dicta expedita quas minima provident.
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nobis quibusdam voluptatibus corporis, harum voluptas ducimus veritatis expedita, ipsam debitis amet, 
+            dignissimos quia aspernatur sapiente quam. Deserunt quibusdam officiis enim error!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo voluptate porro voluptas minus rem. 
+            Blanditiis placeat quo impedit ipsa nam doloremque omnis veritatis est enim.
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque molestias, voluptatum repellat, sunt deleniti, deserunt delectus voluptatibus 
+            aut ratione nesciunt a! Et blanditiis nobis consectetur dicta expedita quas minima provident.
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nobis quibusdam voluptatibus corporis, harum voluptas ducimus veritatis expedita, ipsam debitis amet, 
+            dignissimos quia aspernatur sapiente quam. Deserunt quibusdam officiis enim error!
+        </p>
+</section>
